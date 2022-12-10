eb46b0094edb2bb37558d163c5ecff9828c4e46d
Andrey9595 <anb95.95@mail.ru>
2022-12-10 10:29:36 +0300

    add api

---
+++ app/src/main/java/ru/anb/newsapp/data/api/NewsService.kt
@@ -0,0 +1,19 @@
+package ru.anb.newsapp.data.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.anb.newsapp.utils.Constants.Companion.API_KEY
+
+interface NewsService {
+    @GET("/v2/everything")
+    suspend fun getEverything(
+        @Query("q") query: String,
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+    suspend fun getHeadlines(
+        @Query("country") countryCode :String = "ru",
+        @Query("page") page: Int =1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+}
---
+++ app/src/main/java/ru/anb/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package ru.anb.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "d93229ce05d84c49a5f2d97d0e5b4153"
+    }
+}
